831f661f663c577818d070c45845b5e6fc8fb3de
cheemeburmbger <cheemeburmbger@gmail.com>
2021-05-14 17:52:20 +0300

    Шапка: поменять логотип

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 
 mix.js('src/js/scripts.js', 'js')
-mix.sass('src/sass/styles.scss', 'css');
+mix.sass('src/sass/styles.scss', 'css').options({
+    processCssUrls: false
+});;
 mix.extract(['jquery']);
 mix.setPublicPath('local/templates/main/')
